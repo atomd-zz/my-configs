40864fc06418138156c7123af5ce0b36fcc19d1f
atomd <dwb319@gmail.com>
2014-02-24 00:53:45 +0800

    Add custom file

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -2,16 +2,18 @@
 
 (defvar emacs-home-dir (file-name-directory load-file-name))
 (defvar emacs-core-dir (expand-file-name "core/" emacs-home-dir))
-(add-to-list 'load-path emacs-home-dir)
 (add-to-list 'load-path emacs-core-dir)
+
+(setq custom-file (expand-file-name "custom.el" emacs-home-dir))
+(load custom-file)
 
 ;; the core stuff
 (require 'base)
 (require 'packages)
 (require 'modes)
 
-(require 'ui)
 (require 'editor)
-(require 'kdbs)
 (require 'fonts)
+(require 'kdbs)
+(require 'ui)
 ;;; init.el ends here
